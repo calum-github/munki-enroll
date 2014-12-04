9ff74209884f6c7484445a78da69702225e8eebd
hunty1 <calum@Calum-iMac.local>
2014-12-04 17:54:53 +1100

    First commit
    getting there

--- Scripts/munki_enroll.sh
+++ Scripts/munki_enroll.sh
@@ -1,20 +1,60 @@
 #!/bin/sh 
  
-IDENTIFIER=$( defaults read /Library/Preferences/ManagedInstalls ClientIdentifier ); 
-HOSTNAME=$( scutil --get ComputerName );
+## Munki Enroll - Rebooted!
 
-SUBMITURL="https://munki/munki-enroll/enroll.php"
+# Munki Server URL
+munki_enroll_url="http://munki.test.internal/munki-enroll/enroll.php"
+
+# Target volumes - this should be Macintosh HD - but let DS give us the variable (working on)
+target_volume="/Volumes/Macintosh\ HD"
+
+
+# First lets get some location information - these are used for our directories in /manifests
+region=`grep description /tmp/site_information.txt | cut -f3 -d ","`
+school_name=`grep description /tmp/site_information.txt | cut -f2 -d ","`
+building=`awk -F "," '{print $5}' /tmp/location_details.txt | cut -c 12-`
+level=`awk -F "," '{print $6}' /tmp/location_details.txt | cut -c 9-`
+room=`awk -F "," '{print $7}' /tmp/location_details.txt | cut -c 8-`
+
+# Set the names of our actual manifest files
+region_manifest="_Region_${region}"
+school_manifest="_School_${school_name}"
+building_manifest="_Building_${building}"
+level_manifest="_Level_${level}"
+room_manifest="_Room_${room}"
+
+
+# build our standard client identifier using the wifi mac address and the suffix M - because...reasons
+wifi_mac_address=`networksetup -listallhardwareports | awk '/Hardware Port: Wi-Fi/,/Ethernet/' | awk 'NR==3' | cut -d " " -f 3 | sed s/://g`
+suffix="M"
+hostname="${suffix}-${wifi_mac_address}"
+# Build our full client identifier path minus our hostname
+client_identifier_path="${region}/${school_name}/${building}/${level}/${room}/Clients/"
 
 # Application paths
 CURL="/usr/bin/curl"
+DEFAULTS="/usr/bin/defaults"
 
-$CURL --max-time 5 --silent --get \
-    -d hostname="$HOSTNAME" \
-    -d identifier="$IDENTIFIER" \
-    "$SUBMITURL"
- 	
-IDENTIFIER_PATH=$( echo "$IDENTIFIER" | sed 's/\/[^/]*$//' ); 
- 	
-defaults write /Library/Preferences/ManagedInstalls ClientIdentifier "$IDENTIFIER_PATH/clients/$HOSTNAME"
+#-----------------------------------------------------------------------------------------------------------------#
+
+# Send off our configuration to our server and let it create the directories and manifests if they don't already exist
+$CURL --max-time 5 --get \
+    -d client_identifier_path="$client_identifier_path" \
+    -d hostname="$hostname" \
+    -d region_manifest="$region_manifest" \
+    -d school_manifest="$school_manifest" \
+    -d building_manifest="$building_manifest" \
+    -d level_manifest="$level_manifest" \
+    -d room_manifest="$room_manifest" \
+    -d region="$region" \
+    -d school_name="$school_name" \
+    -d building="$building" \
+    -d level="$level" \
+    -d room="$room" \
+    "$munki_enroll_url"
+
+
+# Now set the ClientIdentifier in the ManagedInstalls plist
+$DEFAULTS write $target_volume/Library/Preferences/ManagedInstalls ClientIdentifier "${client_identifier_path}/${hostname}"
  
 exit 0
--- munki-enroll/enroll.php
+++ munki-enroll/enroll.php
@@ -2,49 +2,79 @@
 
 require_once( 'cfpropertylist-1.1.2/CFPropertyList.php' );
 
-// Get the varibles passed by the enroll script
-$identifier = $_GET["identifier"];
-$hostname   = $_GET["hostname"];
+// Get the variables passed by the enroll script
+// Here we get the path of the clients manifest
+// ie ../manifests/$client_identifier
+// which is ../manifests/$region/$school_name/$building/$level/$room/Clients
+// We have the hostname which will be the name of the clients actual manifest
+$client_identifier_path = $_GET["client_identifier_path"];
+$hostname = $_GET["hostname"];
+//
+// Here need to get the names of the manifests for each folder
+// ie. The manifest that exists in the directory called $school_name 
+// is called $school_manifest
+$region_manifest = $_GET["region_manifest"];
+$school_manifest = $_GET["school_manifest"];
+$building_manifest = $_GET["building_manifest"];
+$level_manifest = $_GET["level_manifest"];
+$room_manifest = $_GET["room_manifest"];
+//
+// Here we need to get the names of the directories inside manifests
+$region = $_GET["region"];
+$school_name = $_GET["school_name"];
+$building = $_GET["building"];
+$level = $_GET["level"];
+$room = $_GET["room"];
 
-// Split the manifest path up to determine directory structure
-$directories		= explode( "/", $identifier, -1 ); 
-$total				= count( $directories );
-$n					= 0;
-$identifier_path	= "";
-while ( $n < $total )
+// Check if manifest already exists for this machine, if not then create
+// The directory structure to support it ie. ../manifests/$region/$school_name/$building/$level/$room/Clients
+ if ( file_exists( '../manifests/' . $client_identifier_path . $hostname ) )
     {
-        $identifier_path .= $directories[$n] . '/';
-        $n++;
-    }
-
-// Check if manifest already exists for this machine
-if ( file_exists( '../manifests/' . $identifier_path . '/clients/' . $hostname ) )
-    {
-        echo "Computer manifest already exists.";
+        echo "Computer manifest already exists. Bailing out here.";
     }
 else
     {
-        echo "Computer manifest does not exist. Will create.";
+        echo "Computer manifest does not exist. Creating directory structure as needed";
         
-        if ( !is_dir( '../manifests/' . $identifier_path . 'clients/' ) )
+        if ( !is_dir( '../manifests/' . $client_identifier_path ) )
             {
-                mkdir( '../manifests/' . $identifier_path . 'clients/', 0755, true );
+                mkdir( '../manifests/' . $client_identifier_path, 0755, true );
             }
-        
-        // Create the new manifest plist
+
+// Now we need to create the computers manifest inside the Clients folder
         $plist = new CFPropertyList();
         $plist->add( $dict = new CFDictionary() );
-        
+      
         // Add manifest to production catalog by default
         $dict->add( 'catalogs', $array = new CFArray() );
         $array->add( new CFString( 'production' ) );
         
         // Add parent manifest to included_manifests to achieve waterfall effect
         $dict->add( 'included_manifests', $array = new CFArray() );
-        $array->add( new CFString( $identifier ) );
+        $array->add( new CFString( "$region/$school_name/$building/$level/$room/$room_manifest" ) );
         
         // Save the newly created plist
-        $plist->saveXML( '../manifests/' . $identifier_path . 'clients/' . $hostname );
+        $plist->saveXML( '../manifests/' . $client_identifier_path . $hostname );
+
+        // 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         
     }
 
